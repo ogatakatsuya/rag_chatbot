85098f8f95be57dadffb5f4ab5195eac2355f57d
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2025-02-06 17:08:29 +0900

    add: Messageをopenaiのtypeにcast

--- src/model.py
+++ src/model.py
@@ -1,7 +1,29 @@
 from pydantic import BaseModel
-from typing import Literal
+from typing import Literal, Union
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionUserMessageParam,
+)
 
 
 class Message(BaseModel):
     role: Literal["user", "assistant", "system"]
     content: str
+
+    def cast_to_openai_schema(
+        self,
+    ) -> Union[
+        ChatCompletionUserMessageParam,
+        ChatCompletionAssistantMessageParam,
+        ChatCompletionSystemMessageParam,
+    ]:
+        """OpenAIのAPIに渡す形式に変換する"""
+        if self.role == "user":
+            return ChatCompletionUserMessageParam(role="user", content=self.content)
+        elif self.role == "assistant":
+            return ChatCompletionAssistantMessageParam(
+                role="assistant", content=self.content
+            )
+        else:
+            return ChatCompletionSystemMessageParam(role="system", content=self.content)
