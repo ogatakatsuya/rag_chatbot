c46728eb37441b8136db5c0fc6c160fb891da885
kawaeeeee <u639748d@ecs.osaka-u.ac.jp>
2025-03-27 05:46:57 +0900

    change client setting

--- rag/data_count.py
+++ rag/data_count.py
@@ -5,13 +5,13 @@ collection_list = client.collections.list_all()
 
 for collection in collection_list:
     print(collection)
-# collection_name = "class_data_full_text"
+collection_name = "class_data_full_text"
 
-# collection = client.collections.get(collection_name)
+collection = client.collections.get(collection_name)
 
-# response = collection.aggregate.over_all(total_count=True)
+response = collection.aggregate.over_all(total_count=True)
 
-# print(f"Total objects in collection '{collection_name}': {response.total_count}")
+print(f"Total objects in collection '{collection_name}': {response.total_count}")
 
 # クライアントを閉じる
 client.close()
