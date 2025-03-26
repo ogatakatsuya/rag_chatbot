86e544b65187b47e2ae74eaa1565429b7cc20b2b
kawaeeeee <u639748d@ecs.osaka-u.ac.jp>
2025-03-27 05:13:06 +0900

    ragsearvice using weaviate

--- rag/data_count.py
+++ rag/data_count.py
@@ -1,13 +1,17 @@
 from rag.client import client
+#デバッグ用
 
+collection_list = client.collections.list_all()
 
-collection_name = "class_data_full_text"
+for collection in collection_list:
+    print(collection)
+# collection_name = "class_data_full_text"
 
-collection = client.collections.get(collection_name)
+# collection = client.collections.get(collection_name)
 
-response = collection.aggregate.over_all(total_count=True)
+# response = collection.aggregate.over_all(total_count=True)
 
-print(f"Total objects in collection '{collection_name}': {response.total_count}")
+# print(f"Total objects in collection '{collection_name}': {response.total_count}")
 
 # クライアントを閉じる
 client.close()
