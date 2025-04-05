16b546c4cbfdad9df735896d762c1c78fa18a71c
kawaeeeee <u639748d@ecs.osaka-u.ac.jp>
2025-04-05 14:42:39 +0900

    insert supabase

--- rag/insert_all.py
+++ rag/insert_all.py
@@ -1,15 +1,14 @@
-from weaviate.collections.collection import Collection
+import sys
+
+import pandas as pd
 import weaviate.classes as wvc
-import re
 
 from rag.client import client
-from rag.model import ClassInfo
-from rag.setup import vector_db_setup, full_text_db_setup
 from rag.insert import insert
-import pandas as pd
+from rag.model import ClassInfo
+from rag.setup import full_text_db_setup, vector_db_setup
 
-import sys
-sys.path.append('../scraping/syllabus')
+sys.path.append("../scraping/syllabus")
 
 
 def load_and_embed_csv():
@@ -17,13 +16,13 @@ def load_and_embed_csv():
     CSVファイルを読み込んで埋め込みを行い、JSONで保存する
     """
     # CSVの読み込み
-    csv_path = '../scraping/syllabus/syllabus.csv'
-    department = '基礎工学部'
+    csv_path = "../scraping/syllabus/syllabus.csv"
+    department = "基礎工学部"
     data = pd.read_csv(csv_path)
 
     # 曜日・時間の整形
     data["day_and_period"] = data["day_and_period"].apply(format_schedule)
-    data['course_code'] = data['course_code'].str.extract(r'(\d+)')
+    data["course_code"] = data["course_code"].str.extract(r"(\d+)")
 
     embeddings = []
     full_texts = []
@@ -33,11 +32,13 @@ def load_and_embed_csv():
     full_text_db_collection = full_text_db_setup("class_data_full_text")
 
     for i, row in data.iterrows():
-        print(row['day_and_period'])
-        #すでに埋め込み済みのデータはスキップ
+        print(row["day_and_period"])
+        # すでに埋め込み済みのデータはスキップ
         result = vector_db_collection.query.fetch_objects(
-            filters=wvc.query.Filter.by_property("course_code").equal(int(row['course_code'])),
-            limit=1
+            filters=wvc.query.Filter.by_property("course_code").equal(
+                int(row["course_code"])
+            ),
+            limit=1,
         )
 
         if result.objects:
@@ -55,27 +56,23 @@ def load_and_embed_csv():
             f"{row['semester']}に開講され、{row['day_and_period']}に行われる。対象は{row['student_year']}生であり、"
             f"本講義の履修には、{row['requirements_prerequisites']}。"
         )
-        date_text = (
-            f"{row['day_and_period']}に開講される授業である。"
-        )
-        content_text = (
-            f"「{row['course_name_jp']}」は、"
-            f"{row['course_objectives']} "
-        )
+        date_text = f"{row['day_and_period']}に開講される授業である。"
+        content_text = f"「{row['course_name_jp']}」は、{row['course_objectives']} "
 
         texts = [full_text, info_text, date_text, content_text]
 
         # # 一番安いモデルで埋め込み
         for text in texts:
-            embeddings.append(ClassInfo(text=text, course_code=int(row['course_code'])))
-        
-        full_texts.append(ClassInfo(text=full_text, course_code=int(row['course_code'])))
+            embeddings.append(ClassInfo(text=text, course_code=int(row["course_code"])))
+
+        full_texts.append(
+            ClassInfo(text=full_text, course_code=int(row["course_code"]))
+        )
 
     # データの挿入
     insert(vector_db_collection, embeddings)
     insert(full_text_db_collection, full_texts)
-    
-    
+
 
 def format_schedule(schedule: str) -> str:
     """
@@ -99,15 +96,14 @@ def format_schedule(schedule: str) -> str:
         "日": "日曜日",
     }
 
-    
     schedule = schedule.strip().replace("\t", "")
 
     parts = schedule.split(",")
     formatted_parts = []
     for part in parts:
-        part = part.strip() 
-        
-        if not part: 
+        part = part.strip()
+
+        if not part:
             continue
 
         day = part[0]
@@ -135,6 +131,5 @@ if __name__ == "__main__":
     result = client.collections.get(collection_name).query.fetch_objects(limit=100)
     print(len(result.objects))
 
-
-    #client終了
+    # client終了
     client.close()
